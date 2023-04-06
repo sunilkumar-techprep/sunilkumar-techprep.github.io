e888372ec94b9b1cd7fc8edca683db46d3e2da5b
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-04-06 17:21:25 +0000

    move file to views folder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,24 +2,24 @@ class ApplicationController < ActionController::Base
 
   def index 
 
-    render({ :template => "layouts/index.html.erb" })
+    render({ :template => "index.html.erb" })
   end 
 def aboutme
-  render({ :template => "layouts/aboutme.html.erb" })
+  render({ :template => "aboutme.html.erb" })
  end
  def contact
-  render({ :template => "layouts/contact.html.erb" })
+  render({ :template => "contact.html.erb" })
  end
  def resume
-  render({ :template => "layouts/resume.html.erb" })
+  render({ :template => "resume.html.erb" })
  end
  def mission
-  render({ :template => "layouts/mission.html.erb" })
+  render({ :template => "mission.html.erb" })
  end
  def portfolio
-  render({ :template => "layouts/portfolio.html.erb" })
+  render({ :template => "portfolio.html.erb" })
  end
  def techskill
-  render({ :template => "layouts/techskill.html.erb" })
+  render({ :template => "techskill.html.erb" })
  end
 end
