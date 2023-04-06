5383a12f25f1ee8fd0401941b2fba100ce746864
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-04-06 18:29:18 +0000

    index.html moves back in docs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,25 @@
 class ApplicationController < ActionController::Base
 
-  def index 
+ #def index 
 
-    render({ :template => "index.html.erb" })
-  end 
-def aboutme
-  render({ :template => "aboutme.html.erb" })
- end
- def contact
-  render({ :template => "contact.html.erb" })
- end
- def resume
-  render({ :template => "resume.html.erb" })
- end
- def mission
-  render({ :template => "mission.html.erb" })
- end
- def portfolio
-  render({ :template => "portfolio.html.erb" })
- end
- def techskill
-  render({ :template => "techskill.html.erb" })
- end
-end
+   # render({ :template => "index.html.erb" })
+  #end 
+#def aboutme
+ # render({ :template => "aboutme.html.erb" })
+# end
+ #def contact
+  #render({ :template => "contact.html.erb" })
+ #end
+ #def resume
+  #render({ :template => "resume.html.erb" })
+# end
+ #def mission
+  #render({ :template => "mission.html.erb" })
+ #end
+ #def portfolio
+  #render({ :template => "portfolio.html.erb" })
+ #end
+ #def techskill
+  #render({ :template => "techskill.html.erb" })
+ #end
+#end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get("/Techskills", { :controller => "application", :action => "techskill" })
+  #get("/Techskills", { :controller => "application", :action => "techskill" })
 
-  get("/", { :controller => "application", :action => "index" })
-  get("/AboutMe", { :controller => "application", :action => "aboutme" })
-  get("/Contact", { :controller => "application", :action => "contact" })
-  get("/Resume", { :controller => "application", :action => "resume" })
-  get("/Mission", { :controller => "application", :action => "mission" })
-  get("/Portfolio", { :controller => "application", :action => "portfolio" })
+  #get("/", { :controller => "application", :action => "index" })
+  #get("/AboutMe", { :controller => "application", :action => "aboutme" })
+  #get("/Contact", { :controller => "application", :action => "contact" })
+  #get("/Resume", { :controller => "application", :action => "resume" })
+  #get("/Mission", { :controller => "application", :action => "mission" })
+  #get("/Portfolio", { :controller => "application", :action => "portfolio" })
 end
